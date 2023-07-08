287a8502ef630ca2d9a38c719b54e236235c0351
EtorosD <123051106+EtorosD@users.noreply.github.com>
2023-07-08 14:32:01 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        id: 1,
+        img: "https://scontent.fcgy2-1.fna.fbcdn.net/v/t1.15752-9/358582177_1313809839524459_4862888157898157021_n.png?_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=hnlahNhrAt8AX8Fw48I&_nc_ht=scontent.fcgy2-1.fna&oh=03_AdSvZGcZWLkV4Kxi4LTJpcQ_IDRdpDZE7TRkSy0tfZj2lw&oe=64D086CB",
+        link: "https://etorosd.github.io/personalblog.github.io/blog.html"
+    },
+
+    {
+        id: 2,
+        img:  "https://scontent.fcgy2-1.fna.fbcdn.net/v/t1.15752-9/357667133_1026286418258995_704082827261798956_n.png?stp=dst-png_p843x403&_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGjF6_q-_OHX4XQ8Tmt6Z5EY_vZt3QQgMxj-9m3dBCAzCd5UnQhGYA0jWOlSm1Tlq56UxtqyvRjF9TxHQTG7HkT&_nc_ohc=xfkoPZLG1JwAX9QRhBH&_nc_ht=scontent.fcgy2-1.fna&oh=03_AdQBsLIvZaL5Kp5GS_U6CV8QfZQS7S_TpIJqgUH22bHieg&oe=64C68F7E",
+        link: "https://etorosd.github.io/petcatalog.github.io/petcatalog.html"
+       
+    },
+
+    {
+        id: 3,
+        img: "https://scontent.fcgy2-2.fna.fbcdn.net/v/t1.15752-9/358875793_1618800721939454_2720664787944608022_n.png?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=hor1_KOtZKgAX-Uzpvd&_nc_ht=scontent.fcgy2-2.fna&oh=03_AdQB8WblVqmN5stO-o-5r53l9Adcplb-rAd5NwvI2dqixg&oe=64D05DD3",
+        link: "https://pabilify-fe-github-io.vercel.app/"
+    }
+]
